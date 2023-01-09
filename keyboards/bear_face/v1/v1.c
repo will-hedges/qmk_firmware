a9fba1e78703ca7aa8fc908d6f29c5d967bd4a3e
Will Hedges <wshedges@outlook.com>
2023-01-08 22:28:08 -0600

    [bear_face/v1] inverted LED pin logic

--- keyboards/bear_face/v1/v1.c
+++ keyboards/bear_face/v1/v1.c
@@ -28,7 +28,8 @@ bool led_update_kb(led_t led_state) {
     // Caps Lock LED indicator toggling code here
     bool res = led_update_user(led_state);
     if(res) {
-        writePin(F7, led_state.caps_lock);
+        // use ! if no MOSFET
+        writePin(F7, !led_state.caps_lock);
     }
     return res;
 }
