35ee62606fcbb38264c8080438416c3d28919bd4
Will Hedges <wshedges@outlook.com>
2023-11-25 15:21:20 -0600

    initial b_box commit

---
+++ keyboards/b_box/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 chemicalwill (@will-hedges)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define WS2812_DI_PIN GP12
+#define RGBLED_NUM 1
+#define RGBLIGHT_LAYERS 3
---
+++ keyboards/b_box/rules.mk
@@ -0,0 +1,4 @@
+RGBLIGHT_ENABLE = yes
+WS2812_DRIVER = vendor
+
+TAP_DANCE_ENABLE = yes
