dbc02f9a5197eaa8c28d7dc6361b5a37a041ee56
Will Hedges <wshedges@outlook.com>
2025-09-11 09:31:00 -0500

    Updated drturklefeet drop/alt

--- keyboards/drop/alt/keymaps/drturklefeet/keymap.c
+++ keyboards/drop/alt/keymaps/drturklefeet/keymap.c
@@ -1,3 +1,18 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _QWER,
+    _FN1,
+    _META
+};
+
+#define FN1_CAPS LT(_FN1, KC_CAPS)
+
+#define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSFT) || get_mods() & MOD_BIT(KC_RSFT))
+#define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTL))
+#define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+
+// ** START MACROS
 enum alt_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
     U_T_AGCR,              //USB Toggle Automatic GCR control
@@ -9,7 +24,7 @@ enum alt_keycodes {
     ARD,
     BHCC,
     BLCC,
-    COMP,
+    _COMP,
     COMP_C,
     COMP_P,
     ESP,
@@ -19,56 +34,15 @@ enum alt_keycodes {
     PC_XFER
 };
 
-// Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
 };
-
-// Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 };
-
-#define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSFT) || get_mods() & MOD_BIT(KC_RSFT))
-#define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTL))
-#define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
 
     switch (keycode) {
-
-        case U_T_AUTO:
-            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
-            }
-            return false;
-
-        case U_T_AGCR:
-            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
-            }
-            return false;
-        case DBG_TOG:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
-            }
-            return false;
-
-        case DBG_MTRX:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
-            }
-            return false;
-
-        case DBG_KBD:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
-            }
-            return false;
-        case DBG_MOU:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
-            }
-            return false;
 
         case MD_BOOT:
             if (record->event.pressed) {
@@ -134,7 +108,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
 
-        case COMP:
+        case _COMP:
             if (record->event.pressed) {
                 SEND_STRING("Was unable to provide the off-label disclaimer to the customer because the customer was not directly contacted.");
             } else {
@@ -199,4 +173,40 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     return true;
+};
+// ** END MACROS
+
+keymap_config_t keymap_config;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWER] = LAYOUT_65_ansi_blocker(
+        KC_ESC,   KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_TAB,   KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
+        FN1_CAPS, KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_QUOT,          KC_ENT,  KC_PGUP, \
+        KC_LSFT,  KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,          KC_UP,   KC_PGDN, \
+        KC_LCTL,  MO(_META), KC_LALT,                            KC_SPC,                               MO(_FN1), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT  \
+    ),
+    [_FN1] = LAYOUT_65_ansi_blocker(
+        KC_GRV,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,  KC_F12,  _______, KC_MUTE, \
+        _______,  RGB_SPD,   RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO, U_T_AGCR, _______, KC_PSCR,  KC_SCRL, KC_PAUS, _______, KC_END,  \
+        _______,  RGB_RMOD,  RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______,  _______,  _______, _______,  _______,          _______, KC_VOLU, \
+        _______,  RGB_TOG,   _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG,  _______,  _______, _______,  _______,          KC_PGUP, KC_VOLD, \
+        _______, _______,    QK_BOOT,                              _______,                            _______,  _______, KC_HOME, KC_PGDN, KC_END   \
+    ),
+    [_META] = LAYOUT_65_ansi_blocker(
+        _______,  _______,   _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______, _______, _______, ARD,     \
+        _______,  _______,   _______, ESP,     PAP,     _______, _______, COMP_P,   _COMP,    COMP_C,  PC_XFER,  _______, _______, _______, BHCC,    \
+        _______,  NARR,      _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______,          _______, PAP2,    \
+        _______,  _______,   _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______,          _______, BLCC,    \
+        _______,  _______,   _______,                            _______,                              _______,  _______, _______, _______, _______  \
+    ),
+    /*
+    [X] = LAYOUT(
+        _______,  _______,   _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______, _______, _______, _______, \
+        _______,  _______,   _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______, _______, _______, _______, \
+        _______,  _______,   _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______,          _______, _______, \
+        _______,  _______,   _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______,          _______, _______, \
+        _______,  _______,   _______,                            _______,                              _______,  _______, _______, _______, _______  \
+    ),
+    */
 };
